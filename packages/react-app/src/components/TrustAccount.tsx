0b79c728083c77b888f2128f87d2f8b3d289e1b9
aman sagar <amansagar262@gmail.com>
2024-11-17 00:21:12 +0700

    chore: modify screen`

--- packages/react-app/src/components/TrustAccount.tsx
+++ packages/react-app/src/components/TrustAccount.tsx
@@ -2,24 +2,27 @@ import { useMemo } from "react";
 import Blockies from "react-blockies";
 
 interface ProfileCardProps {
+  address: string;
   name?: string;
 }
+export const randomWalletAddress = () => {
+  const chars = "0123456789abcdef";
+  let address = "0x";
+  for (let i = 0; i < 40; i++) {
+    address += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return address;
+};
 
-export default function TrustAccount({ name = "John Doe" }: ProfileCardProps) {
-  const randomWalletAddress = useMemo(() => {
-    const chars = "0123456789abcdef";
-    let address = "0x";
-    for (let i = 0; i < 40; i++) {
-      address += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return address;
-  }, []);
-
+export default function TrustAccount({
+  address,
+  name = "aman",
+}: ProfileCardProps) {
   return (
-    <div className="flex items-center p-4 border rounded-lg">
+    <div className="flex items-centerp-4 rounded-lg px-4">
       {/* Profile Picture */}
       <Blockies
-        seed={randomWalletAddress.toLowerCase()}
+        seed={address.toLowerCase()}
         size={10}
         scale={5}
         className="rounded-full"
@@ -27,9 +30,11 @@ export default function TrustAccount({ name = "John Doe" }: ProfileCardProps) {
 
       {/* Name and Wallet */}
       <div className="ml-4 flex flex-col">
-        <span className="font-medium text-lg">{name}</span>
+        <span className="font-medium text-lg">
+          {name || truncateAddress(address)}
+        </span>
         <span className="text-sm text-gray-500 mt-1">
-          {truncateAddress(randomWalletAddress)}
+          {truncateAddress(address)}
         </span>
       </div>
     </div>
--- packages/react-app/src/screens/Home.tsx
+++ packages/react-app/src/screens/Home.tsx
@@ -1,26 +1,44 @@
-import TrustAccount from "@/components/TrustAccount";
-import { CiUser, CiLocationArrow1 } from "react-icons/ci";
+import TrustAccount, { randomWalletAddress } from "@/components/TrustAccount";
+import { QRCodeSVG } from "qrcode.react";
 
 export default function Home() {
   return (
-    <div>
-      <TrustAccount />
-      <div className="py-4">
-        <div className="flex justify-between items-center">
+    <div className="flex flex-col gap-4 w-full items-center">
+      <TrustAccount address={randomWalletAddress()} />
+
+      <QRCodeSVG
+        className=" rounded-lg p-2 shadow-xl"
+        value={randomWalletAddress()}
+        size={200}
+        level="H"
+        includeMargin={true}
+      />
+
+      <div className="py-4 flex gap-4">
+        <div className="flex justify-between items-center flex-col">
+          <span>Trust Score</span>
+          <span className=" text-xl text-[#36B82A]">10/10</span>
+        </div>
+
+        <div className="flex justify-between items-center flex-col">
+          <span>Balance</span>
+          <span className=" text-xl text-[#36B82A]">$7,782.00</span>
+        </div>
+        {/* <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
             <CiUser />
             <p className="text-lg">Total Supporters</p>
           </div>
           <p className="font-bold text-lg">15</p>
-        </div>
+        </div> */}
 
-        <div className="flex justify-between items-center">
+        {/* <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
             <CiLocationArrow1 />
             <p className="text-lg">Total Amount</p>
           </div>
           <p className="font-bold text-lg">$12,345</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
