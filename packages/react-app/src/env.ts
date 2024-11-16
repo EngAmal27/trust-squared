dfe603b0d767d361addb65d89e6f1576099cd9df
aman sagar <amansagar262@gmail.com>
2024-11-17 06:55:45 +0700

    chore: fix env

--- packages/react-app/src/env.ts
+++ packages/react-app/src/env.ts
@@ -1,4 +1,4 @@
-export const POOL_CONTRACT = import.meta.env.POOL_CONTRACT;
-export const SF_FORWARDER = import.meta.env.SF_FORWARDER;
-export const GOODDOLLAR = import.meta.env.GOODDOLLAR;
-export const API_URL = import.meta.env.API_URL;
+export const POOL_CONTRACT = import.meta.env.VITE_POOL_CONTRACT;
+export const SF_FORWARDER = import.meta.env.VITE_SF_FORWARDER;
+export const GOODDOLLAR = import.meta.env.VITE_GOODDOLLAR;
+export const API_URL = import.meta.env.VITE_API_URL;
