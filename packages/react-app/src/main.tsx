b3818121c44890aadc51d60c264d2b4b4fff0b39
aman sagar <amansagar262@gmail.com>
2024-11-17 07:53:14 +0700

    chore: debug

--- packages/react-app/src/main.tsx
+++ packages/react-app/src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import WalletProvider from "./providers/dynamic.tsx";
-// import { Toaster } from "./components/ui/toaster.tsx";
-
+import WalletProvider from "./providers/dynamic.tsx"; 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WalletProvider>
